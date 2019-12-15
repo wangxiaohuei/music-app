4f6307bdee5f8bc5f66bab290a77c4eed379c002
王聪慧 <wangconghuei@sina.cn>
2019-12-15 19:37:08 +0800

    歌手列表页

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -20,6 +20,11 @@ export default {
     data: {
       type: Array,
       default: null
+    },
+    // scroll 是否需要监听滚动事件
+    listenScroll: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
@@ -41,6 +46,13 @@ export default {
         probeType: this.probeType,
         click: this.click
       })
+
+      if (this.listenScroll) {
+        let me = this
+        this.scroll.on('scroll', (pos) => {
+          me.$emit('scroll', pos)
+        })
+      }
     },
     enable() {
       this.scroll && this.scroll.enable()
@@ -50,6 +62,12 @@ export default {
     },
     refresh() {
       this.scroll && this.scroll.refresh()
+    },
+    scrollTo() {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement() {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
     }
   },
   watch: {
